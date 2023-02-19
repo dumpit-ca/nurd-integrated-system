457e1d101df3d994d776fdb889c6b8ca907f23b0
dumpit-ca <dumpitca@outlook.com>
2023-02-20 00:15:25 +0800

    added NAvbr component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,31 @@
+import logo from '../../src/images/nurd-logo.png';
 const Navbar = () => {
-   return (  );
-}
- 
+   return (
+      <nav className="navbar flex">
+         <a href="">
+            <img src={logo} alt="NURD Logo" />
+         </a>
+         <div className="nav-link">
+            <ul className="flex flex-row">
+               <li>
+                  <a href="#">About</a>
+               </li>
+               <li>
+                  <a href="#">Events</a>
+               </li>
+               <li>
+                  <a href="#">Announcements</a>
+               </li>
+               <li>
+                  <a href="#">Forum</a>
+               </li>
+               <li>
+                  <a href="#">Contact Us</a>
+               </li>
+            </ul>
+         </div>
+      </nav>
+   );
+};
+
 export default Navbar;
