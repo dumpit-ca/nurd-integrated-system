4630849a516abac4d4ea8892b634c3688e5b7fc9
cass <dumpitca@outlook.com>
2023-02-21 17:25:23 +0800

    added frontend/client side folder

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import '../src/assets/sass/main.scss';
+
+function App() {
+   return (
+      <div className="container App">
+         <Navbar />
+         <div className="content"></div>
+      </div>
+   );
+}
+
+export default App;
