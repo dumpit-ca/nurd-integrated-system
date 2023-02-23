6a531a8b8ba2390dcd6561a7eca12036de1305ce
cass <dumpitca@outlook.com>
2023-02-23 17:07:12 +0800

    added footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "../src/assets/sass/main.scss";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
       <div className="App">
          <Navbar />
          <div className="content"></div>
+         <Footer />
       </div>
    );
 }
