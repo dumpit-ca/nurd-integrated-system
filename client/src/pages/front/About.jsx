f923e0c2b4da4b8724890ce18b08c313d32e3990
Cass <dumpitca@outlook.com>
2023-03-02 12:46:44 +0800

    added hero header to about page

--- client/src/pages/front/About.jsx
+++ client/src/pages/front/About.jsx
@@ -1,6 +1,18 @@
-import { useState } from "react";
+// import { useState } from 'react';
 const About = () => {
-   return <div className="about"></div>;
+   return (
+      <div className='container mx-auto px-5'>
+         <div className='about'>
+            <div className='grid grid-cols-2 gap-5'>
+               <div className='hero-text flex flex-col'>
+                  <h1 className='hero-text-header'>
+                     Powering innovation with imagination.
+                  </h1>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default About;
