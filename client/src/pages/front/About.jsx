53187acd8f57a28a274df925a6b60028d251ab41
cass <dumpitca@outlook.com>
2023-03-04 02:45:03 +0800

    Added hero image in About page

--- client/src/pages/front/About.jsx
+++ client/src/pages/front/About.jsx
@@ -1,12 +1,19 @@
+import Hero from "../../assets/images/hero-img.png";
 const About = () => {
    return (
-      <div className="container mx-auto py-5">
-         <div className="about">
-            <div className="hero-header">
-               <h1 className="hero-header-text text-4xl">
-                  Powering <br /> innovation with <br />
-                  imagination.
-               </h1>
+      <div className="container mx-auto">
+         <div className="md:py-5 md:px-0 p-5 about">
+            <div className="grid md:grid-cols-2 md:place-items-center md:gap-0 gap-4 grid-cols-1 hero-header">
+               <div className="hero-header-text">
+                  <h1 className="text-5xl w-50">
+                     Powering <br /> innovation with <br />
+                     imagination.
+                  </h1>
+                  <p className="hero-header-subheader text-lg w-96">The NU Research Department empowers every researcher and provides support towards innovative ideas that will revolutionize industries.</p>
+               </div>
+               <div className="hero-header-img">
+                  <img src={Hero} className="hero-img" alt="NU Hero Image" />
+               </div>
             </div>
          </div>
       </div>
