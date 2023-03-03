a5164ac386a832418f3432fe6f510f69e3b1beae
cass <dumpitca@outlook.com>
2023-03-04 02:00:23 +0800

    Created About page

--- client/src/pages/front/About.jsx
+++ client/src/pages/front/About.jsx
@@ -1,6 +1,16 @@
-import { useState } from "react";
 const About = () => {
-   return <div className="about"></div>;
+   return (
+      <div className="container mx-auto py-5">
+         <div className="about">
+            <div className="hero-header">
+               <h1 className="hero-header-text text-4xl">
+                  Powering <br /> innovation with <br />
+                  imagination.
+               </h1>
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default About;
