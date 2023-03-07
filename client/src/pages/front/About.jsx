1fcaac7e8286c45f19e21ddbb3e8666f1a7b9da9
cass <dumpitca@outlook.com>
2023-03-07 22:36:52 +0800

    Added linear gradient to hero image

--- client/src/pages/front/About.jsx
+++ client/src/pages/front/About.jsx
@@ -1,28 +1,20 @@
-import Hero from "../../assets/images/hero-img.png";
-import logo from "../../assets/images/nurd-logo.png";
 const About = () => {
    return (
-      <div className="container mx-auto">
-         <div className="md:py-5 md:px-0 p-5 about">
-            <div className="grid md:grid-cols-2 md:place-items-center md:gap-0 gap-4 grid-cols-1 hero-header">
-               <div className="hero-header-text">
-                  <h1 className="text-5xl w-50">
-                     Powering <br /> innovation with <br />
-                     imagination.
+      <div className="container mx-auto py-10">
+         <div className="hero-img-container relative">
+            <div className="hero-inner-container flex justify-center items-center h-full w-full z-10 absolute">
+               <div className="text-center">
+                  <h1 className="text-5xl">
+                     Powering <span className="uppercase">INNOVATION</span>
+                     <br />
+                     with <span className="uppercase">IMAGINATION</span>
                   </h1>
-                  <p className="hero-header-subheader text-lg w-96">The NU Research Department empowers every researcher and provides support towards innovative ideas that will revolutionize industries.</p>
-               </div>
-               <div className="hero-header-img">
-                  <img src={Hero} className="hero-img" alt="NU Hero Image" />
-               </div>
-            </div>
-            <div className="md:my-24 md:flex gap-24 items-center md:justify-center">
-               <img src={logo} className="about-header-img" alt="" />
-               <div className="line"></div>
-               <div className="about-header">
-                  <h1 className="md:text-6xl text-lg">About Us</h1>
+                  <p className="text-base mt-10">The NU Research Department empowers every researcher and provides support towards innovative ideas that will revolutionize industries.</p>
+                  {/* <a href="">LOGIN</a>
+                  <a href="">BROWSE FORUMS</a> */}
                </div>
             </div>
+            <div className="hero-overlay absolute w-full bottom-0"></div>
          </div>
       </div>
    );
