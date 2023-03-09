8da17ff68d1e9d38f40ee3bafe6f18cfd03310b4
Cass <dumpitca@outlook.com>
2023-03-09 21:28:20 +0800

    Fixing styling

--- client/src/pages/front/About.jsx
+++ client/src/pages/front/About.jsx
@@ -1,23 +1,30 @@
 const About = () => {
-   return (
-      <div className="container mx-auto py-10">
-         <div className="hero-img-container relative">
-            <div className="hero-inner-container flex justify-center items-center h-full w-full z-10 absolute">
-               <div className="text-center">
-                  <h1 className="text-5xl">
-                     Powering <span className="uppercase">INNOVATION</span>
-                     <br />
-                     with <span className="uppercase">IMAGINATION</span>
-                  </h1>
-                  <p className="text-base mt-10">The NU Research Department empowers every researcher and provides support towards innovative ideas that will revolutionize industries.</p>
-                  {/* <a href="">LOGIN</a>
+  return (
+    <div className="container mx-auto py-10">
+      <div className="hero-img-container relative">
+        <div className="hero-inner-container flex justify-center items-center h-full w-full z-10 absolute">
+          <div className="text-center md:p-0 p-5">
+            <h1 className="text-5xl">
+              Powering <span className="uppercase">INNOVATION</span>
+              <br />
+              with <span className="uppercase">IMAGINATION</span>
+            </h1>
+            <p className="hero-inner-text md:text-lg text-base mt-10">
+              The NU Research Department empowers every researcher and provides
+              support towards innovative ideas that will revolutionize
+              industries.
+            </p>
+            {/* <div className="grid gap-2 md:block">
+                     <a href="" className="btn"></a>
+                  </div> */}
+            {/* <a href="">LOGIN</a>
                   <a href="">BROWSE FORUMS</a> */}
-               </div>
-            </div>
-            <div className="hero-overlay absolute w-full bottom-0"></div>
-         </div>
+          </div>
+        </div>
+        <div className="hero-overlay absolute w-full bottom-0"></div>
       </div>
-   );
+    </div>
+  );
 };
 
 export default About;
